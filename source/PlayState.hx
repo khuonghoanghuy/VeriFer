8e3c0a21e764841debee006c6959f53255c8f0f0
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-01-28 11:57:47 +0700

    added infoTxt

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,10 +3,17 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxMath;
+import flixel.system.FlxAssets;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	var player:FlxSprite;
+	// pretty hud
+	var infoTxt:FlxText;
+	var score:Int = 0;
 
 	override public function create()
 	{
@@ -15,11 +22,26 @@ class PlayState extends FlxState
 		player.makeGraphic(16, 16, 0xffff0000);
 		player.active = true;
 		add(player);
+		infoTxt = new FlxText(10, 10, 0, "Score: " + score, 16);
+		infoTxt.active = true;
+		infoTxt.setFormat(FlxAssets.FONT_DEFAULT, 16, FlxColor.WHITE, LEFT, OUTLINE_FAST, FlxColor.BLACK);
+		add(infoTxt);
+	}
+
+	function updateInfoTxt()
+	{
+		infoTxt.text = "Score: " + FlxMath.lerp(0, score, 0.1);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			score += 100;
+			updateInfoTxt();
+		}
+
 		if (FlxG.keys.anyPressed([LEFT, A]))
 		{
 			player.velocity.x = -50;
