92ce29446d515b9649e79def6a3c39f43556be6d
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-01-28 11:52:12 +0700

    simply moving system

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 
@@ -19,5 +20,29 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		if (FlxG.keys.anyPressed([LEFT, A]))
+		{
+			player.velocity.x = -50;
+		}
+		else if (FlxG.keys.anyPressed([RIGHT, D]))
+		{
+			player.velocity.x = 50;
+		}
+		else
+		{
+			player.velocity.x = 0;
+		}
+
+		if (FlxG.keys.anyJustPressed([UP, W]))
+		{
+			player.velocity.y = -50;
+			player.acceleration.y = -200;
+		}
+		else
+		{
+			player.acceleration.y = 0;
+		}
+
+		player.acceleration.y += 400;
 	}
 }
